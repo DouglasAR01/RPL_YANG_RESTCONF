06cf7099441cdd463152153aaee760c89faad979
Douglas Ramírez <33143337+Doukuest@users.noreply.github.com>
2020-03-09 18:06:58 -0500

    Primera versión funcional con confdata
    Aún falta hacer state data de ejemplo y terminar de hacer el backend.

--- Backend/Empalme/usr_init.py
+++ Backend/Empalme/usr_init.py
@@ -2,8 +2,8 @@ from colorlog import info
 
 def jc_startup():
     #Código a ejecutar cuando inicia el servidor via Jetconf
-    info("Backend: Enpalme iniciado con éxito.")
+    info("Backend: Empalme iniciado con éxito.")
 
 def jc_end():
     #Código a ejecutar cuando el servidor es apagado
-    info("Backend: Enpalme cerrado.")
+    info("Backend: Empalme cerrado.")
